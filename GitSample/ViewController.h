d84a14a67222970410414ee164aca224c884a70d
abhiii <abhii.reddy6@gmail.com>
2013-06-05 18:32:52 +0530

    my first commit

---
+++ GitSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitSample
+//
+//  Created by Hadi Hatunoglu on 05/06/13.
+//  Copyright (c) 2013 Hadi Hatunoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitSample/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  GitSample
+//
+//  Created by Hadi Hatunoglu on 05/06/13.
+//  Copyright (c) 2013 Hadi Hatunoglu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"log one");
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
